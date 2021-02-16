f3fd81a85540e005b8779188721a8ab32a52df4e
Sarah Carlson <sbcarls@gmail.com>
2021-02-16 12:30:05 -0500

    updated code to work correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var appid = "0cbb9ca46e3a74d91c3901d2c3bf89b7";
-    var $searchInput = $(".search");
+    var $searchInput = $("#search");
     var $searchBtn = $("#searchBtn");
     var $currentDay = $(".jumbotron");
     var $fiveDayForecast = $(".fiveDayForecast");
@@ -17,6 +17,7 @@ $(document).ready(function(){
 
     function getWeatherForCity(city) {
         $(".jumbotron").empty()
+        console.log("hello");
         $.ajax({
             method:"GET", 
             url:"https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=0cbb9ca46e3a74d91c3901d2c3bf89b7"
@@ -28,20 +29,40 @@ $(document).ready(function(){
             $(".jumbotron").append(cityName,temp,humidity)
            
         });
-
-        $searchBtn.on("click", function(){
-            if($searchBtn.val()){
-                getWeatherForCity($searchInput.val());
-                $searchInput.val("");
-            }
-        });
      
     }
+
+    $searchBtn.on("click", function(event){
+        event.preventDefault();
+        console.log($searchInput.val());
+        if($searchInput.val()){
+            console.log("trigger");
+            getWeatherForCity($searchInput.val());
+            fiveDay($searchInput.val());
+            $searchInput.val("");
+        }
+    });
    
     
+    function fiveDay(city) {
+        $fiveDayForecast.show();
+        $.ajax({
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=0cbb9ca46e3a74d91c3901d2c3bf89b7",
+            method: "GET",
+        }) .then(res => {
+            console.log(res.list);
+            const fiveDayArray = [res.list[2], res.list[10], res.list[18], res.list[26], res.list[34]]
+            for(i = 0; i < fiveDayArray.length; i++) {
+                var temp = $("<p>").text("Temperature: " + fiveDayArray[i].main.temp)
+                $(".day" + (i+1).toString()).append(temp)
+            }
+        })
+
+    }
 
      //$cardBody.hide();
-     //$fiveDayForecast.hide();
+    
+     $fiveDayForecast.hide();
 });
 
 
